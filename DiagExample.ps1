76bf190e58350566bcff393fc553387e98af0f78
VictorLegat <38977389+VictorLegat@users.noreply.github.com>
2019-11-29 13:47:59 +0200

    20191129 update 3

--- DiagExample.ps1
+++ DiagExample.ps1
@@ -14,4 +14,9 @@ Connect-O365PS exo
 
 
 Publish-Module -Name "O365Troubleshooters" -Path "C:\Users\vilega\Documents\GitHub\O365Troubleshooters\"  -NuGetApiKey "oy2h3ibtm6jsynifz5mqmm6at5gkownefjuz7urc7qwlgm" -ProjectUri "https://github.com/VictorLegat/O365Troubleshooters"
+
+Publish-Module -Name "O365Troubleshooters"  -NuGetApiKey "oy2h3ibtm6jsynifz5mqmm6at5gkownefjuz7urc7qwlgm" -ProjectUri "https://github.com/VictorLegat/O365Troubleshooters"
+
+New-ModuleManifest -Path "C:\Program Files\WindowsPowerShell\Modules\O365Troubleshooters\O365Troubleshooters.psd1"
+Update-ModuleManifest -ProjectUri "https://github.com/VictorLegat/O365Troubleshooters" -Path "C:\Program Files\WindowsPowerShell\Modules\O365Troubleshooters\O365Troubleshooters.psd1"
 Register-PSRepository -Name "O365Troubleshooters" -SourceLocation "https://github.com/VictorLegat/O365Troubleshooters" -PublishLocation "https://github.com/VictorLegat/O365Troubleshooters" -InstallationPolicy Trusted
