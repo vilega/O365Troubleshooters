e1b77cb6c6e68e87242e8293c45e074e0780850d
VictorLegat <38977389+VictorLegat@users.noreply.github.com>
2019-11-29 11:19:06 +0200

    20191129 Initial

--- DiagExample.ps1
+++ DiagExample.ps1
@@ -2,7 +2,7 @@
 Write-Host "Test"
 Import-Module 'C:\Users\vilega\Documents\GitHub\O365Troubleshooters\CommonFunctions.psm1' -DisableNameChecking
 
-Import-Module CommonFunctions
+Import-Module CommonFunctions.psm1 -Force
 CommonFunctions\MENU
 
 Write-Host "After module import"
