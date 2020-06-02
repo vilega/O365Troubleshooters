71224849463289293f6af198ba2144172b99b934
Liviu Nita <alexandru.nita@microsoft.com>
2020-06-02 10:48:22 +0300

    test local beta repository
    added cmdlet to test local repository

--- test.ps1
+++ test.ps1
@@ -1,6 +1,11 @@
 Import-Module "C:\Users\vilega\Documents\GitHub\O365Troubleshooters\O365Troubleshooters.psm1" -Force
 Import-Module "C:\Users\alnita\Documents\GitHub\O365Troubleshooters\O365Troubleshooters.psm1" -Force
 
+##Test with Local Beta Version instead of Powershell Galery Version
+Start-Process powershell.exe -ArgumentList "-noexit -Command `
+Import-Module C:\Users\alnita\Documents\GitHub\O365Troubleshooters\O365Troubleshooters.psm1 -Force; `
+Set-GlobalVariables; Start-O365TroubleshootersMenu" -Verb RunAs -Wait
+
 Start-O365Troubleshooters
 
 Set-GlobalVariables
