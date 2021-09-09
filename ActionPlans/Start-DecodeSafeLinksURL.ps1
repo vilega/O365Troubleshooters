e2203ecc2f9f9e74cd71502d6e6c72b43f6e19a1
Victor Legat <victor.legat@microsoft.com>
2021-09-09 12:50:33 +0300

    Fix bug 167

--- ActionPlans/Start-DecodeSafeLinksURL.ps1
+++ ActionPlans/Start-DecodeSafeLinksURL.ps1
@@ -1,4 +1,10 @@
+#.SYN...
+
+
+#
 Clear-Host
+
+# 
 $encodedURL  = Read-Host("Please provide the ATP SafeLinks URL that you want to decode to original URL")
 Add-Type -AssemblyName System.Web
 
@@ -6,27 +12,37 @@ Add-Type -AssemblyName System.Web
 try
 {   # Decode URL using UrlDecode from System.Web.HttpUtility
     $decodedURL = [System.Web.HttpUtility]::UrlDecode($encodedURL)
+    
+    
     #$decodedURL = (($decodedURL -Split "url=")[1] -split "&data=;")[0]
-    if($decodedURL -match ".safelinks.protection.outlook.com\/\?url=.+&data=")
+    
+    # 
+    if($decodedURL -match ".safelinks.protection.outlook.com\/.*\?url=.+&data=")
     {
-        $decodedURL = $Matches[$Matches.Count - 1]
-        $decodedURL = (($decodedURL -Split "protection.outlook.com\/\?url=")[1] -Split "&data=")[0]
+        $decodedURL = (($decodedURL -Split "/?url=")[1] -Split "&data=")[0]
     }
-    elseif($decodedURL -match ".safelinks.protection.outlook.com\/\?url=.+&amp;data=")
+    elseif($decodedURL -match ".safelinks.protection.outlook.com\/.*\?url=.+&amp;data=")
     {
-        $decodedURL = $Matches[$Matches.Count - 1]
-        $decodedURL = (($decodedURL -Split "protection.outlook.com\/\?url=")[1] -Split "&amp;data=")[0]
+        $decodedURL = (($decodedURL -Split "/?url=")[1] -Split "&amp;data=")[0]
     }
     else{throw "InvalidSafeLinksURL"}
 }
-catch
+#
+catch [InvalidSafeLinksURL]
 {
     Write-Log -function "Start-AP_DecodeSafeLinksURL" -step  "Decoding URL" -Description "Couldn't decode and parse URL: $encodedURL"
     Write-Host "Couldn't decode and parse URL: $encodedURL"
     Read-Host "Press any key and then to reload main menu [Enter]"
     Start-O365TroubleshootersMenu
 }
+catch
+{
 
+}
+
+# ask for new decode
+
+# HTML & TXT
 Write-Host "The decoded URL is:" -ForegroundColor Green
 Write-Host $decodedURL
 Write-Log -function "Start-AP_DecodeSafeLinksURL" -step  "Decoding URL" -Description "Decoded and Parse URL is: $decodedURL"
