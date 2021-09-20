8045dbfbc5cb90126dbc9410d9c8a0fa0982c9f8
Alexandru Catalin <alexcatalin_24@yahoo.com>
2021-09-20 17:44:30 +0300

    Update Start-CheckMBXPermissions.ps1
    Commit 3

--- ActionPlans/Start-CheckMBXPermissions.ps1
+++ ActionPlans/Start-CheckMBXPermissions.ps1
@@ -4,6 +4,57 @@ Import-Module C:\Users\alexaca\Documents\GitHub\O365Troubleshooters\O365Troubles
 Set-GlobalVariables
 Start-O365TroubleshootersMenu
 #>
+
+
+
+function Get-SharedMailboxFolderPermission {
+
+    #Extracting the Name attribute for the mailbox
+    
+    $alias=(Get-Mailbox $MBX).Name.ToString()
+    
+    #Extracting the Primary SMTP Address for the mailbox
+    
+    $SMTP =(Get-Mailbox $MBX).PrimarySMTPAddress.ToString()
+    
+    #Getting the default mailbox folders list
+    
+    $folders=get-mailbox $MBX | Get-MailboxFolderStatistics | ? {($_.FolderType -eq "Inbox") -or ($_.FolderType -eq "Archive") -or ($_.FolderType -eq "Drafts") -or ($_.FolderType -eq "Outbox") -or ($_.FolderType -eq "Calendar") -or ($_.FolderType -eq "SentItems") -or ($_.FolderType -eq "Contacts") -or ($_.FolderType -eq "Tasks") -or ($_.FolderType -eq "Notes") -or ($_.FolderType -eq "DeletedItems") -or ($_.FolderType -eq "JunkEmail")} | select Identity
+    
+    $rights = @()
+    
+    #Adjusting the folder Identity values obtained by previous command, to comply with the Get-MailboxFolderPermission cmdlet required format. Getting the folder permissions as well.
+    
+    foreach ($folder in $folders) {
+    
+    $foldername = $folder.Identity.ToString().Replace([char]63743,"/").Replace($alias,$SMTP + ":")
+    
+    try
+    
+    {
+    
+    $MBrights = Get-MailboxFolderPermission -Identity "$foldername" -ErrorAction Stop
+    
+    $MBrights =$MBrights | Select FolderName,User, AccessRights, @{Name = 'SMTP'; Expression = {$SMTP}}
+    
+    #With below 2 command lines I am attempting to get the Top of Information Store folder permission as well in the mailbox.
+    
+    $MBRightsRoot = Get-MailboxFolderPermission -Identity "$MBX" -ErrorAction Stop
+    
+    $MBRightsRoot = $MBRightsRoot | Select FolderName,User, AccessRights, @{Name = 'SMTP'; Expression = {$SMTP}}
+    
+    $rights += $MBrights
+    }
+    
+    Catch {}
+    
+    }
+    
+    return ($rights + $MBRightsRoot)
+    
+    }
+
+
 # connect
 Clear-Host
 $Workloads = "exo"
@@ -19,8 +70,35 @@ $ExportPath = "$global:WSPath\MailboxDiagnosticLogs_$ts"
 mkdir $ExportPath -Force |out-null
 
 
-$mbx = Read-Host "SMTP "
-write-host (Get-Mailbox $mbx).Name
+#Gathering a list of the User Principal Name attribute for the specified recipient type
+
+Write-Host "======================" -ForegroundColor Gray
+
+Write-Host "SharedMailbox" -ForegroundColor Green
+
+Write-Host "UserMailbox" -ForegroundColor Yellow
+
+Write-Host "RoomMailbox" -ForegroundColor Cyan
+
+Write-Host "EquipmentMailbox" -ForegroundColor Magenta
+
+Write-Host "======================" -ForegroundColor Gray
+
+$Type=Read-Host -Prompt "Please type the RecipientTypeDetails for which you wish to get the information, available values are shown above"
+
+$UPN = Get-Mailbox -RecipientTypeDetails $Type -ResultSize Unlimited | select UserPrincipalName
+
+$UPN=($UPN).UserPrincipalName
+
+foreach ($MBX in $UPN) {
+
+
+#Export a CSV file for each mailbox, that contains the default folders, their associated permissions and the Primary SMTP address of the mailbox in question.
+
+
+Get-SharedMailboxFolderPermission | Export-Csv -Path "$(($MBX.ToString()))_MailboxFolderPermissions.csv" -NoTypeInformation -Encoding UTF8 -UseCulture
+
+}
 
 
 
