f8cf3389d7d487e6b384c11f02f5d0238f0c4cf0
Alexandru Catalin <alexcatalin_24@yahoo.com>
2021-09-28 13:50:51 +0300

    Implemented action plan partial logging completed

--- ActionPlans/Start-CheckMBXPermissions.ps1
+++ ActionPlans/Start-CheckMBXPermissions.ps1
@@ -6,99 +6,54 @@ Start-O365TroubleshootersMenu
 #>
 
 
-<#
-function Get-AllNoneUserDefaultMailboxFolderPermissions {
-
-    #Extracting the Name attribute for the mailbox
-    
-    $alias = (Get-Mailbox $MBX).Name.ToString()
-    
-    #Extracting the Primary SMTP Address for the mailbox
-    
-    $SMTP = (Get-Mailbox $MBX).PrimarySMTPAddress.ToString()
-    
-    #Getting the default mailbox folders list
-    
-    $folders = get-mailbox $MBX | Get-MailboxFolderStatistics | ? { ($_.FolderType -eq "Inbox") -or ($_.FolderType -eq "Archive") -or ($_.FolderType -eq "Drafts") -or ($_.FolderType -eq "Outbox") -or ($_.FolderType -eq "Calendar") -or ($_.FolderType -eq "SentItems") -or ($_.FolderType -eq "Contacts") -or ($_.FolderType -eq "Tasks") -or ($_.FolderType -eq "Notes") -or ($_.FolderType -eq "DeletedItems") -or ($_.FolderType -eq "JunkEmail") } | select Identity
-    
-    $rights = @()
-    
-    #Adjusting the folder Identity values obtained by previous command, to comply with the Get-MailboxFolderPermission cmdlet required format. Getting the folder permissions as well.
-    
-    foreach ($folder in $folders) {
-    
-        $foldername = $folder.Identity.ToString().Replace([char]63743, "/").Replace($alias, $SMTP + ":")
-    
-        try {
-    
-            $MBrights = Get-MailboxFolderPermission -Identity "$foldername" -ErrorAction Stop | ? { ($_.User.tostring() -eq "Default") -and !($_.AccessRights.contains("None")) }
-    
-            $MBrights = $MBrights | Select FolderName, User, AccessRights, @{Name = 'SMTP'; Expression = { $SMTP } }
-    
-            #With below 2 command lines I am attempting to get the Top of Information Store folder permission as well in the mailbox.
-    
-            $MBRightsRoot = Get-MailboxFolderPermission -Identity "$MBX" -ErrorAction Stop | ? { ($_.User.tostring() -eq "Default") -and !($_.AccessRights.contains("None")) }
-    
-            $MBRightsRoot = $MBRightsRoot | Select FolderName, User, AccessRights, @{Name = 'SMTP'; Expression = { $SMTP } }
-    
-            $rights += $MBrights
-        }
-    
-        Catch {}
-    
-    }
-    
-    return ($rights + $MBRightsRoot)
-    
-}#>
-
-
 function Get-AllDefaultUserMailboxFolderPermissions {
 
-    #Extracting the Name attribute for the mailbox
+    param(
+        [System.Collections.ArrayList]$MBXs,
+        [bool]$isDefaultFolder)
     
-    $alias = (Get-Mailbox $MBX).Name.ToString()
     
-    #Extracting the Primary SMTP Address for the mailbox
+    $rights = New-Object -TypeName "System.Collections.ArrayList"
+    $foldersForAllMbx = @()
     
-    $SMTP = (Get-Mailbox $MBX).PrimarySMTPAddress.ToString()
-    
-    #Getting the default mailbox folders list
-    
-    $folders = get-mailbox $MBX | Get-MailboxFolderStatistics | ? { ($_.FolderType -eq "Inbox") -or ($_.FolderType -eq "Archive") -or ($_.FolderType -eq "Drafts") -or ($_.FolderType -eq "Outbox") -or ($_.FolderType -eq "Calendar") -or ($_.FolderType -eq "SentItems") -or ($_.FolderType -eq "Contacts") -or ($_.FolderType -eq "Tasks") -or ($_.FolderType -eq "Notes") -or ($_.FolderType -eq "DeletedItems") -or ($_.FolderType -eq "JunkEmail") } | select Identity
-    
-    $rights = @()
-    
-    #Adjusting the folder Identity values obtained by previous command, to comply with the Get-MailboxFolderPermission cmdlet required format. Getting the folder permissions as well.
-    
-    foreach ($folder in $folders) {
-    
-        $foldername = $folder.Identity.ToString().Replace([char]63743, "/").Replace($alias, $SMTP + ":")
-    
-        try {
-    
-            $MBrights = Get-MailboxFolderPermission -Identity "$foldername" -ErrorAction Stop
-    
-            $MBrights = $MBrights | Select FolderName, User, AccessRights, @{Name = 'SMTP'; Expression = { $SMTP } }
-    
-            #With below 2 command lines I am attempting to get the Top of Information Store folder permission as well in the mailbox.
-    
-            $MBRightsRoot = Get-MailboxFolderPermission -Identity "$MBX" -ErrorAction Stop
-    
-            $MBRightsRoot = $MBRightsRoot | Select FolderName, User, AccessRights, @{Name = 'SMTP'; Expression = { $SMTP } }
-    
-            $rights += $MBrights
+    foreach ($MBX in $MBXs) {
+        #Extracting the Name attribute for the mailbox
+        $alias = (Get-Mailbox $MBX).Name.ToString()
+        #Extracting the Primary SMTP Address for the mailbox
+        $SMTP = (Get-Mailbox $MBX).PrimarySMTPAddress.ToString()
+        #Getting all mailbox folders list
+        if ($isDefaultFolder -eq $true) {
+            [System.Collections.ArrayList]$folders = get-mailbox $MBX | Get-MailboxFolderStatistics | Where-Object FolderType -ne "User Created" | select Identity, @{Name = 'Alias'; Expression = { $alias } } , @{Name = 'SMTP'; Expression = { $SMTP } } 
         }
-    
-        Catch {}
-    
+        else {
+            [System.Collections.ArrayList]$folders = get-mailbox $MBX | Get-MailboxFolderStatistics | select Identity, @{Name = 'Alias'; Expression = { $alias } } , @{Name = 'SMTP'; Expression = { $SMTP } } 
+        }
+        $foldersForAllMbx += $folders
     }
     
-    return ($rights + $MBRightsRoot)
     
+        
+        
+    #Adjusting the folder Identity values obtained by previous command, to comply with the Get-MailboxFolderPermission cmdlet required format. Getting the folder permissions as well.
+    foreach ($folder in $foldersForAllMbx) {
+        $foldername = $folder.Identity.ToString().Replace([char]63743, "/").Replace($folder.alias, $folder.SMTP + ":")
+        try {
+            $MBrights = Get-MailboxFolderPermission -Identity "$foldername" -ErrorAction Stop
+            [System.Collections.ArrayList]$MBrights = $MBrights | Select FolderName, User, AccessRights, @{Name = 'SMTP'; Expression = { $SMTP } }
+            #With below 2 command lines I am attempting to get the Top of Information Store folder permission as well in the mailbox.
+            $MBRightsRoot = Get-MailboxFolderPermission -Identity "$MBX" -ErrorAction Stop
+            [System.Collections.ArrayList]$MBRightsRoot = $MBRightsRoot | Select FolderName, User, AccessRights, @{Name = 'SMTP'; Expression = { $SMTP } }
+            foreach ($entry in $MBRightsRoot) {
+                $null = $MBrights.Add($entry)
+            }
+                
+            $null = $rights.Add($MBrights)
+        }
+        Catch {}
+    }
+    return ($rights)
 }
-
-
+    
 # connect
 Clear-Host
 $Workloads = "exo"
@@ -114,45 +69,34 @@ $ExportPath = "$global:WSPath\MailboxDiagnosticLogs_$ts"
 mkdir $ExportPath -Force | out-null
 
 
-#Gathering a list of the User Principal Name attribute for the specified recipient type
-
-Write-Host "======================" -ForegroundColor Gray
-
-Write-Host "SharedMailbox" -ForegroundColor Green
-
-Write-Host "UserMailbox" -ForegroundColor Yellow
-
-Write-Host "RoomMailbox" -ForegroundColor Cyan
-
-Write-Host "EquipmentMailbox" -ForegroundColor Magenta
-
-Write-Host "======================" -ForegroundColor Gray
-
-$Type = Read-Host -Prompt "Please type the RecipientTypeDetails for which you wish to get the information, available values are shown above"
-$options = "SharedMailbox", "UserMailbox", "RoomMailbox", "EquipmentMailbox", "Specific SMTP Address"
-$Type = $options | out-gridview -Title "Please select...." -outputmode single
-if ($Type -eq "Specific SMTP Address") {
-    $address = Read-Host "Please type the full SMTP Address" 
-    $UPN = Get-Mailbox -Identity $address | select UserPrincipalName
-
+$allMBX = Get-ExoMailbox -Filter "RecipientTypeDetails -eq 'UserMailbox' -or RecipientTypeDetails -eq 'SharedMailbox'" | select DisplayName, PrimarySmtpAddress, UserPrincipalName
+Write-Host "Warning: Please keep in mind that the more mailboxes are selected, this will affect the performance of the script" -ForegroundColor Yellow
+$choice = Read-Host "Please select the mailboxes that need to be checked (press Enter to display the list of mailboxes)"
+$allMBXInitialCount = $allMBX.Count
+[Array]$allMBX = ($allMBX | select DisplayName, PrimarySmtpAddress, UserPrincipalName | Out-GridView -PassThru -Title "Select one or more..").PrimarySmtpAddress
+$allMBXSelectedCount = $allMBX.Count
+    
+If ($allMBXSelectedCount -eq 0) {
+    # go to the menu or get again all mbx
 }
-
-else {
-    $UPN = Get-Mailbox -RecipientTypeDetails $Type -ResultSize Unlimited | select UserPrincipalName
-    $UPN = ($UPN).UserPrincipalName
+    
+Write-Host "Warning: Depending on the number of mailboxes selected, running the script to check all folders, might give a timeout" -ForegroundColor Yellow
+#$choice = Read-Host "Do you want to check all folders or only default ones? Input '1' for 'All folders' or '2' for 'Default folders'"
+$choice = Get-Choice -Options 'All Folders', 'Default Folders'   
+if ($choice -eq "d") {
+    $isDefaultFolder = $true
 }
-
-
-foreach ($MBX in $UPN) {
-
-
-    #Export a CSV file for each mailbox, that contains the default folders, their associated permissions and the Primary SMTP address of the mailbox in question.
-
-
-    Get-AllDefaultUserMailboxFolderPermissions | Export-Csv -Path "$(($MBX.ToString()))_MailboxFolderPermissions.csv" -NoTypeInformation -Encoding UTF8 -UseCulture
-
-
+elseif ($choice -eq "a") {
+    $isDefaultFolder = $false
 }
+    
+$rights = Get-AllDefaultUserMailboxFolderPermissions -MBXs $allMBX -isDefaultFolder $isDefaultFolder
+
+$ExportRights = $rights | % { $_ }
+
+$ExportRights | Export-Csv $ExportPath\Mailbox_Folder_Permissions_$ts.csv -NoTypeInformation
+
+#Start-Process     C:\Temp\permissions.csv
 
 
 Read-Key
