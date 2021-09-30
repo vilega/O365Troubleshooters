12ba76d3d9446a80a84507c7471def0d0b3a6ae6
LaurentiuAlbu <49229889+LaurentiuAlbu@users.noreply.github.com>
2021-09-30 13:03:06 +0300

    added error control, integration and some logging

--- ActionPlans/Start-ComplianceSearchBulkDelete.ps1
+++ ActionPlans/Start-ComplianceSearchBulkDelete.ps1
@@ -14,17 +14,32 @@
         Online documentation: https://answers.microsoft.com/
 
     #>
-    Clear-Host
+<#
+    Import-Module C:\Work\Projects\PS\GitHubStuff\O365Troubleshooters\O365Troubleshooters.psm1 -Force
 
+# 2nd requirement Execute set global variables
+
+Set-GlobalVariables
+
+# 3rd requirement to start the menu
+
+Start-O365TroubleshootersMenu
+
+#>
+
+Clear-Host
+$Workloads = "exo", "SCC"
+   
+Connect-O365PS $Workloads
 
 ### Scenario A.1. - Single mailbox scenario - Delete search items that are accessible to user
 
-    # Select the search from existing searches
+# Select the search from existing searches
 
 $allSearches = Get-ComplianceSearch
 Write-Host -ForegroundColor Yellow "Please select the search for which you wish to delete the found items:"
  
-[string]$SelectedSearch = ($allSearches | Select-Object name |Out-GridView -OutputMode single -Title "Select one search").Name
+[string]$SelectedSearch = ($allSearches | Select-Object name | Out-GridView -OutputMode single -Title "Select one search").Name
 
 $ComplianceSearch = Get-ComplianceSearch -Identity $SelectedSearch
 
@@ -35,42 +50,44 @@ $searchname = $ComplianceSearch.Name
 Write-Host "Found " -NoNewline; Write-Host -ForegroundColor Yellow "$initialitems " -NoNewline; Write-Host -ForegroundColor white "items for compliance Search " -NoNewline; Write-Host -ForegroundColor yellow "$searchname"
 
 
-    # Confirmation input - to be replaced with 'Choice' function
+# Confirmation input - to be replaced with 'Choice' function
 
-    Write-Host -ForegroundColor Magenta "Are you sure you want to delete $initialitems items found by the selected $searchname Compliance Search?"
-    [string]$Option = read-host "Type 'yes' to confirm"
+Write-Host -ForegroundColor Magenta "Are you sure you want to delete $initialitems items found by the selected $searchname Compliance Search?"
 
-    If ($Option -ne "yes") {exit}
+# [string]$option = get-choice "yes", "no"
+    
+[string]$Option = read-host "Type 'yes' to confirm"
+$option = $option.ToLower()
+If ($Option -ne "yes") { exit }
 
-    # Identifying 'Recoverable Items', 'Purges' and 'Versions' folders 
+# Identifying 'Recoverable Items', 'Purges' and 'Versions' folders - this part is taken from article:  
 
-Write-Host "Identifying 'Recoverable Items', 'Deletions', 'Purges' and 'Versions' folders"
+Write-Host "Identifying 'Recoverable Items', 'Purges' and 'Versions' folders"
 
 [string]$mbx = $compliancesearch.exchangelocation # to add check to confirm there is a single mailbox in locations
 $folderQueries = @()
-   $folderStatistics = Get-MailboxFolderStatistics $mbx | where-object {($_.FolderPath -eq "/Recoverable Items") -or ($_.FolderPath -eq "/Purges") -or ($_.FolderPath -eq "/Versions")}
-   foreach ($folderStatistic in $folderStatistics)
-   {
-       $folderId = $folderStatistic.FolderId;
-       $folderPath = $folderStatistic.FolderPath;
-       $encoding= [System.Text.Encoding]::GetEncoding("us-ascii")
-       $nibbler= $encoding.GetBytes("0123456789ABCDEF");
-       $folderIdBytes = [Convert]::FromBase64String($folderId);
-       $indexIdBytes = New-Object byte[] 48;
-       $indexIdIdx=0;
-       $folderIdBytes | Select-Object -skip 23 -First 24 | %{$indexIdBytes[$indexIdIdx++]=$nibbler[$_ -shr 4];$indexIdBytes[$indexIdIdx++]=$nibbler[$_ -band 0xF]}
-       $folderQuery = "folderid:$($encoding.GetString($indexIdBytes))";
-       $folderStat = New-Object PSObject
-       Add-Member -InputObject $folderStat -MemberType NoteProperty -Name FolderPath -Value $folderPath
-       Add-Member -InputObject $folderStat -MemberType NoteProperty -Name FolderQuery -Value $folderQuery
-       $folderQueries += $folderStat
-   }
+$folderStatistics = Get-MailboxFolderStatistics $mbx | where-object { ($_.FolderPath -eq "/Recoverable Items") -or ($_.FolderPath -eq "/Purges") -or ($_.FolderPath -eq "/Versions") }
+foreach ($folderStatistic in $folderStatistics) {
+    $folderId = $folderStatistic.FolderId;
+    $folderPath = $folderStatistic.FolderPath;
+    $encoding = [System.Text.Encoding]::GetEncoding("us-ascii")
+    $nibbler = $encoding.GetBytes("0123456789ABCDEF");
+    $folderIdBytes = [Convert]::FromBase64String($folderId);
+    $indexIdBytes = New-Object byte[] 48;
+    $indexIdIdx = 0;
+    $folderIdBytes | Select-Object -skip 23 -First 24 | % { $indexIdBytes[$indexIdIdx++] = $nibbler[$_ -shr 4]; $indexIdBytes[$indexIdIdx++] = $nibbler[$_ -band 0xF] }
+    $folderQuery = "folderid:$($encoding.GetString($indexIdBytes))";
+    $folderStat = New-Object PSObject
+    Add-Member -InputObject $folderStat -MemberType NoteProperty -Name FolderPath -Value $folderPath
+    Add-Member -InputObject $folderStat -MemberType NoteProperty -Name FolderQuery -Value $folderQuery
+    $folderQueries += $folderStat
+}
    
-    $RecoverableItemsFolder = $folderQueries.folderquery[0]
-    $PurgesFolder = $folderQueries.folderquery[1]
-    $VersionsFolder = $folderQueries.folderquery[2]
+$RecoverableItemsFolder = $folderQueries.folderquery[0]
+$PurgesFolder = $folderQueries.folderquery[1]
+$VersionsFolder = $folderQueries.folderquery[2]
 
-    # Adjusting the search scope to exclude 'Recoverable Items', 'Purges' and 'Versions' folders 
+# Adjusting the search scope to exclude 'Recoverable Items', 'Purges' and 'Versions' folders 
 
 Write-Host -ForegroundColor Yellow "Adjusting the search scope to exclude 'Recoverable Items', 'Purges' and 'Versions' folders"
 
@@ -82,47 +99,69 @@ Set-ComplianceSearch $searchname -ContentMatchQuery $NewContentMatchQuery
 $Iterations = [math]::Ceiling($initialitems / 10)
 $iteration = 0
 
-    # Starting bulk deletion
+# Starting bulk deletion
 
 write-host "Deleting " -NoNewline; Write-Host -ForegroundColor Yellow "$initialitems" -NoNewline; Write-Host -ForegroundColor white " items in " -NoNewline; Write-Host -ForegroundColor yellow "$iterations" -NoNewline; Write-Host -ForegroundColor white " batches of 10 items each, due to Compliance Search Action limit"
 
 DO {
 
-   $iteration++
+    $iteration++
 
-   Write-Host "Refreshing the Compliance Search"
+    Write-Host "Refreshing the Compliance Search"
 
-        Get-ComplianceSearch $searchname | Start-ComplianceSearch
-        $PurgeAction = Get-ComplianceSearchAction | Where-Object {($_.Name -match "$searchname") -and ($_.Name -match "_Purge")}
-        If ($PurgeAction) {
-            foreach ($p in $PurgeAction) {Remove-ComplianceSearchAction ($p).name -Confirm:$False | Out-Null}
-            }
+    Get-ComplianceSearch $searchname | Start-ComplianceSearch
+    $PurgeAction = Get-ComplianceSearchAction | Where-Object { ($_.Name -match "$searchname") -and ($_.Name -match "_Purge") }
+    If ($PurgeAction) {
+        foreach ($p in $PurgeAction) { Remove-ComplianceSearchAction ($p).name -Confirm:$False | Out-Null }
+    }
                         
-            Do {
-                $search = Get-ComplianceSearch $searchname
-                Write-Host " > current Search status is $($search.Status) and search job progress: $($search.JobProgress)%"
-                Start-Sleep -Seconds 5
-            } While (($search.Status -ne 'Completed') -and ($search.JobProgress -ne '100'))
+    Do {
+        $search = Get-ComplianceSearch $searchname
+        Write-Host " > current Search status is $($search.Status) and search job progress: $($search.JobProgress)%"
+        Start-Sleep -Seconds 5
+    } While (($search.Status -ne 'Completed') -and ($search.JobProgress -ne '100'))
 
-        Start-Sleep -Seconds 10
-        $items = (Get-ComplianceSearch $searchname).Items
-        Write-Progress -Activity "Purging $initialitems items" -Status "$items items left" -PercentComplete ($Iteration / $Iterations * 100)
+    Start-Sleep -Seconds 5
+    $items = (Get-ComplianceSearch $searchname).Items
+    Write-Progress -Activity "Purging $initialitems items" -Status "$items items left" -PercentComplete ($Iteration / $Iterations * 100)
 
-        Write-Host -ForegroundColor Yellow "Batch no. [$Iteration / $Iterations]"
-        New-ComplianceSearchAction -SearchName $searchname -Purge -PurgeType HardDelete -Confirm:$false | Out-Null
+    Write-Host -ForegroundColor Yellow "Batch no. [$Iteration / $Iterations]"
+    [boolean]$repeat = $True
+    $i = 1
+    while ($repeat) {
+        
+        try {
+            New-ComplianceSearchAction -SearchName $searchname -Purge -PurgeType HardDelete -Confirm:$false | Out-Null
+            $repeat=$false
+        }
+        catch {
+            Start-Sleep -Seconds 5
+            Write-Log -function "Start-complianceSearchBulkDelete" -step  "Create new Compliance Search Action" -Description "$($PSItem.Exception.Message)"
+            if ($i -lt 5) {
+            $i++
+            } 
+            else {
+                Write-Host "cannot create new search action, returning to menu"
+                Read-Key
+                Start-O365TroubleshootersMenu
+                # choice: call cleanup function
+            }
+        }
+    }   
 
-            Do {
-                $PurgeAction = Get-ComplianceSearchAction -Identity "$searchname`_Purge"
-                Write-Host " > current batch Purge Action status: $($PurgeAction.Status)"
-                Start-Sleep -Seconds 5
+    
+    Do {
+        $PurgeAction = Get-ComplianceSearchAction -Identity "$searchname`_Purge"
+        Write-Host " > current batch Purge Action status: $($PurgeAction.Status)"
+        Start-Sleep -Seconds 5
 
-            } While ($PurgeAction.Status -ne 'Completed')
+    } While ($PurgeAction.Status -ne 'Completed')
 
-        Remove-ComplianceSearchAction ($PurgeAction).name -Confirm:$False | Out-Null
+    Remove-ComplianceSearchAction ($PurgeAction).name -Confirm:$False | Out-Null
 
 } While ($iteration -ne $Iterations)
 
-Remove-ComplianceSearchAction ($PurgeAction).name -Confirm:$False | Out-Null
+# choice: call cleanup funcion
 
 Write-Host -ForegroundColor Yellow "Finished deleting $initialitems items found by Compliance Search $searchname!"
 
