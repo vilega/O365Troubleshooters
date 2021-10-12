3b1c7c3b6aecacde806e4997701472b6bdb8cd56
hazemembaby <71065889+hazemembaby@users.noreply.github.com>
2021-10-12 16:06:01 +0200

    Add Html report for diag 11 & 12

--- ActionPlans/Start-FindUserWithSpecificRbacRole.ps1
+++ ActionPlans/Start-FindUserWithSpecificRbacRole.ps1
@@ -1,15 +1,45 @@
+<# 1st requirement install the module O365 TS
+Import-Module C:\Users\haembab\Documents\GitHub\O365Troubleshooters\O365Troubleshooters.psm1 -Force
+# 2nd requirement Execute set global variables
+Set-GlobalVariables
+# 3rd requirement to start the menu
+Start-O365TroubleshootersMenu
+#>
 Clear-Host
 $Workloads = "exo"
 Connect-O365PS $Workloads
 $CurrentProperty = "Connecting to: $Workloads"
 $CurrentDescription = "Success"
 write-log -Function "Connecting to O365 workloads" -Step $CurrentProperty -Description $CurrentDescription 
-    
+Clear-Host
 $ts= get-date -Format yyyyMMdd_HHmmss
 $ExportPath = "$global:WSPath\RbacRoleSpecific_$ts"
 mkdir $ExportPath -Force | Out-Null
 . $script:modulePath\ActionPlans\Start-RbacTools.ps1
-Get-SpecificRoleMembers |export-csv "$ExportPath\RoleMembers_$ts.csv" -NoTypeInformation 
-Write-Host "The list of user who have selected roles assigned was exported to $ExportPath."
+#HTML Report
+[System.Collections.ArrayList]$TheObjectToConvertToHTML = @()
+$SpecificRoleMembers=Get-SpecificRoleMembers
+[string]$SectionTitle = "Introduction"
+[String]$article='<a href="https://docs.microsoft.com/en-us/exchange/understanding-role-based-access-control-exchange-2013-help" target="_blank">Understanding role based access control</a>'
+[string]$Description = "This report spans all management roles across your enviroment to list users member of a selected exchange online organization Role "+"<b>$($SpecificRoleMembers.role[0].tostring())</b>"+", for more information on RBAC please check the following article: $article"
+[PSCustomObject]$StartHTML = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -Description $Description -DataType "String" -EffectiveDataString "Please check the next section for more information!"
+$null = $TheObjectToConvertToHTML.Add($StartHTML)
+[string]$SectionTitle = "Management Role Assignment Users Table"
+[string]$Description = "This section lists a table with the tenant users & their corresponding management RBAC roles."
+[PSCustomObject]$RBACHTML = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $SpecificRoleMembers -TableType Table
+$null = $TheObjectToConvertToHTML.Add($RBACHTML)
+#region ResultReport
+[string]$FilePath = $ExportPath + "\ManagementRoleAssignmentUsers.html"
+Export-ReportToHTML -FilePath $FilePath -PageTitle "Management Role Assignment Users" -ReportTitle "Management Role Assignment Users" -TheObjectToConvertToHTML $TheObjectToConvertToHTML
+#Question to ask enduser for opening the HTMl report
+$OpenHTMLfile=Read-Host "Do you wish to open HTML report file now?`nType Y(Yes) to open or N(No) to exit!"
+if ($OpenHTMLfile -like "*y*")
+{
+Write-Host "Opening report...." -ForegroundColor Cyan
+Start-Process $FilePath
+}
+#endregion ResultReport
+$SpecificRoleMembers|export-csv "$ExportPath\RoleMembers_$ts.csv" -NoTypeInformation 
+Write-Host "`nOutput was exported in the following location: $ExportPath" -ForegroundColor Yellow
 Read-Key
 Start-O365TroubleshootersMenu
--- ActionPlans/Start-RbacTools.ps1
+++ ActionPlans/Start-RbacTools.ps1
@@ -6,13 +6,13 @@ Function Get-SpecificRoleMembers {
         $roles = (Get-ManagementRole | Select-Object name | Sort-Object name  |Out-GridView -PassThru -Title "List users that have the role you selected:").Name
     }
     $GetEffectiveUsers = Get-ManagementRoleAssignment -GetEffectiveUsers | Where-Object {(($_.Enabled -eq $True) -and ($roles -match $_.Role))} |`
-        Select-Object Role, RoleAssigneeName, RoleAssigneeType, RoleAssignmentDelegationtype, User, CustomRecipientWriteScope, CustomConfigWriteScope, RecipientWriteScope, ConfigWriteScope, Identity 
+        Select-Object Role, RoleAssigneeName, RoleAssigneeType, RoleAssignmentDelegationtype, @{ Name = 'Alias(User)'; Expression = {$_.User}}, CustomRecipientWriteScope, CustomConfigWriteScope, RecipientWriteScope, ConfigWriteScope, Identity 
     return $GetEffectiveUsers
 }
 
 Function Get-AllUsersWithAllRoles {
 
     $GetEffectiveUsers = Get-ManagementRoleAssignment -GetEffectiveUsers | Where-Object {($_.Enabled -eq $True)} |`
-        Select-Object Role, RoleAssigneeName, RoleAssigneeType, RoleAssignmentDelegationtype, User, CustomRecipientWriteScope, CustomConfigWriteScope, RecipientWriteScope, ConfigWriteScope, Identity 
+        Select-Object Role, RoleAssigneeName, RoleAssigneeType, RoleAssignmentDelegationtype, @{ Name = 'Alias(User)'; Expression = {$_.User}}, CustomRecipientWriteScope, CustomConfigWriteScope, RecipientWriteScope, ConfigWriteScope, Identity 
     return $GetEffectiveUsers
 }
