1b9b85a526e55c2899c54e630b50cbfb5fdb3bc5
roansmarques <roansmarques@gmail.com>
2021-10-13 19:44:24 -0300

    MVP2 - Checker for Holds and Distribution issues

--- ActionPlans/Start-SPORetentionChecker.ps1
+++ ActionPlans/Start-SPORetentionChecker.ps1
@@ -11,25 +11,29 @@ ok 1 - Enumerate common issues this script may spot
    2 - Add output to highlight the issues and guide fixes
 ok 3 - Integrate with main menu
 ok 4 - Integrate with S&C connection function --> Pending function fix worked by Victor
-   5 - Code the issues checks
+ok 5 - Code the issues checks scoped for hackathon
    6 - Create wiki for the Action Plan
    7 - Create the help info using the template
+   x - Add eDiscovery case name and ID to CaseHold items
    x - Automate adding exception for a site?
    x - Automate adding exception for a M365 group?
 
 1 - Enumerate common issues this script may spot
     ok  a. Sharepoint holds over ODB sites
     ok  b. M365 group holds over SPO sites
-        c. Distribution issues checking status for Mode, DistributionStatus, DistributionResults
+    ok  c. Distribution issues checking status for Mode, DistributionStatus, DistributionResults
         d. Show policies protecting a site --> Create issue for later (not in hackathon sprint) 
 
 2 - Add output to highlight the issues and guide fixes
         a. Add references about how to setup the exclusions
+        b. Explain about the grace-period for disabled policies
+        c. Add reference to the article to remove inconsistent policies
+        d. Add reference about retry distribution
 
-5 - Code the issues checks
+5 - Code the issues checks scoped for hackathon
     ok  a. Sharepoint holds over ODB sites
     ok  b. M365 group holds over SPO sites
-        c. Distribution issues checking status for Mode, DistributionStatus, DistributionResults
+    ok  c. Distribution issues checking status for Mode, DistributionStatus, DistributionResults
 
 6 - Create wiki for the Action Plan
     Word document + share with Victor
@@ -42,7 +46,9 @@ Questions:
     Closed   2 - Export CSV should be optional? --> Export when not empty
     Closed   3 - Make the report more inclusive including signs rather than colors only --> We'll create an 'issue' to build this after the hackathon 
     Closed   4 - PS session crashing --> Due to the module design, it won't happen in prod experience
-    
+
+Issues:
+             1 - Script does not identify Enabled policies which have the workload disabled (hold will remain during grace-period)
 #>
 #endregion ------------------------------------------------------------
 
@@ -54,7 +60,7 @@ Connect-IPPSSession -UserPrincipalName roan@roanmarques.onmicrosoft.com -Prefix 
 -------------------------------------------------------------------   #>
 
 #Connect-O365PS "SCC"
-#Connect-IPPSSession -Prefix cc
+Connect-IPPSSession -Prefix cc
 #Connect-IPPSSession -UserPrincipalName roan@roanmarques.onmicrosoft.com -Prefix cc
 
 
@@ -74,10 +80,7 @@ catch {
     Start-O365TroubleshootersMenu
 }
 
-
-
-# -----------------------------------------------------
-
+#region Get data
 # Initialization variables
 $workloads = @("SharePoint","OneDrive","ModernGroup") # SharePoint, OneDrive, ModernGroup (Microsoft 365 Groups)
 $Report = New-Object -TypeName "System.Collections.ArrayList"
@@ -109,6 +112,7 @@ Foreach ($workload in $workloads){
                   Mode                = $P.Mode
                   DistributionStatus  = $P.DistributionStatus
                   DistributionResults = $P.DistributionResults
+                  Enabled             = $P.Enabled
                   }
                 $Report += $ReportLine 
 
@@ -127,6 +131,7 @@ Foreach ($workload in $workloads){
                                             Mode                = $P.Mode
                                             DistributionStatus  = $P.DistributionStatus
                                             DistributionResults = $P.DistributionResults
+                                            Enabled             = $P.Enabled
                                         }
                             $Report += $ReportLine
                         }
@@ -147,18 +152,23 @@ Foreach ($workload in $workloads){
                                     Mode                = $P.Mode
                                     DistributionStatus  = $P.DistributionStatus
                                     DistributionResults = $P.DistributionResults
+                                    Enabled             = $P.Enabled
                                     }
                     $Report += $ReportLine  
                 }                    
             }
     }
 }
+#endregion Get data
 
+#region Treat data
+# Shape Report Source Objects
 $HoldsReport = $Report | Select-Object PolicyName, SiteName, Address, Workload, Type, Guid
 $PoliciesReport = $Report | 
                     Sort-Object Guid -Unique | 
-                        Select-Object PolicyName, Type, Guid, Mode, DistributionStatus, DistributionResults
-          
+                        Select-Object PolicyName, Type, Enabled, Guid, Mode, DistributionStatus, DistributionResults
+# Removes 'DistributionResults' to avoid polluting the report
+$PoliciesLeanReport = $PoliciesReport | Select-Object PolicyName, Type, Enabled, Guid, Mode, DistributionStatus
 
 # Filter SharePoint Holds - SharePoint + M365 groups
 $SPOReport = New-Object -TypeName "System.Collections.ArrayList"
@@ -172,8 +182,28 @@ $ODBReport += $HoldsReport | Where-Object {$_.Workload -eq "SharePoint" -and $_.
 $ODBReport += $HoldsReport | Where-Object {$_.Workload -eq "OneDrive"}
 $ODBReport = $ODBReport | Sort-Object -Descending "SiteName" | Sort-Object "PolicyName"
 
+# Filter Healthy Policies
+$HealthyPolicies = New-Object -TypeName "System.Collections.ArrayList"
+$HealthyPolicies += $PoliciesLeanReport | 
+                        Where-Object {
+                            $_.DistributionStatus -eq "Success" -and
+                            $_.DistributionResult.count -eq 0 -and
+                            $_.Mode -eq "Enforce"
+                        }
+                        #$Policies[0] | FL
 
-#region Prepare HTML Report
+# Filter Distribution issues (Mode not: 'Enforce'; Status not: 'Success';  Results not: empty)
+$DistributionIssues = New-Object -TypeName "System.Collections.ArrayList"
+$DistributionIssues += $PoliciesReport | 
+                        Where-Object {
+                            $_.DistributionStatus -ne "Success" -or
+                            $_.DistributionResult.count -gt 0 -or 
+                            $_.Mode -ne "Enforce"
+                        }
+
+#endregion Treat data
+
+#region Present data
 $TheObjectToConvertToHTML = New-Object -TypeName "System.Collections.ArrayList"
 
 # HTML sample: Start-DlToO365GroupUpgradeChecks
@@ -185,26 +215,44 @@ $TheObjectToConvertToHTML = New-Object -TypeName "System.Collections.ArrayList"
 [PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -DataType "String" -EffectiveDataString $Description
 $null = $TheObjectToConvertToHTML.Add($SectionHtml)
 
-# Adds the session for distribution status
+# Adds the session for healthy policies  in case there is any
 #TODO: check if there is content
-[string]$SectionTitle = "Policies Distribution Status"
-[string]$Description = "Check the status for each policy distribution"
-[PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $PoliciesReport -TableType Table
-$null = $TheObjectToConvertToHTML.Add($SectionHtml)
+If ($HealthyPolicies.Count -gt 0){
+    [string]$SectionTitle = "Healthy Policies"
+    [string]$Description = "These policies were checked for common distribution problems and no issue were found. Note: Disabled policies may still enforce holds due to the grace-period."
+    [PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Green" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $HealthyPolicies -TableType Table
+    $null = $TheObjectToConvertToHTML.Add($SectionHtml)
+}
+
+# Adds the session for distribution issues in case there is any
+If ($DistributionIssues.Count -gt 0){
+    # Adds the session for distribution status
+    #TODO: check if there is content
+    [string]$SectionTitle = "Policies Distribution Issues"
+    [string]$Description = "The policies below are in an inconsistent status, review and consider fixing the issues pointed out in DISTRIBUTIONRESULTS column. Then, retry the distribution."
+    [PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Red" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $DistributionIssues -TableType Table
+    $null = $TheObjectToConvertToHTML.Add($SectionHtml)
+}
 
 # Adds the session for policies affecting SharePoint
 #TODO: check if there is content
 [string]$SectionTitle = "SharePoint Holds"
-[string]$Description = "These are the holds which may be preventing SharePoint files and sites to be deleted. Sites connected to a M365 group are impacted by ModernGroup holds."
-#[PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Red" -Description $Description -DataType "String" -EffectiveDataString "Bla bla"
+If ($SPOReport.Count -gt 0){
+    [string]$Description = "These are the holds which may be preventing SharePoint files and sites to be deleted. Sites connected to a M365 group are impacted by ModernGroup holds."
+} Else {
+    [string]$Description = "No retetion policies or eDiscovery case holds were found that could be preventing SharePoint files and sites to be deleted. In case you still face issues consider checking retention labels and disabled policies in grace-period."
+}
 [PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $SPOReport -TableType Table
-#[PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Red" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $Report -TableType List
 $null = $TheObjectToConvertToHTML.Add($SectionHtml)
 
 # Adds the session for policies affecting OneDrive
 #TODO: check if there is content
 [string]$SectionTitle = "OneDrive Holds"
-[string]$Description = "These are the holds which may be preventing OneDrive files and sites to be deleted."
+If ($ODBReport.Count -gt 0){   
+    [string]$Description = "These are the holds which may be preventing OneDrive files and sites to be deleted."
+} Else {
+    [string]$Description = "No retetion policies or eDiscovery case holds were found that could be preventing OneDrive files and sites to be deleted. In case you still face issues consider checking retention labels and disabled policies in grace-period."
+}
 [PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $ODBReport -TableType Table
 $null = $TheObjectToConvertToHTML.Add($SectionHtml)
 
@@ -224,7 +272,7 @@ if ($OpenHTMLfile.ToLower() -like "*y*") {
     Write-Host "Opening report...." -ForegroundColor Cyan
     Start-Process $FilePath
 }
-#endregion Prepare HTML Report
+#endregion Present data
     
 # Print location where the data was exported
 Write-Host "`nOutput was exported in the following location: $ExportPath" -ForegroundColor Yellow 
