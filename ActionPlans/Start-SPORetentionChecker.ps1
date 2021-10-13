ce5c853f0ddadd1eba1a05322b67a4b210ab19a6
roansmarques <roansmarques@gmail.com>
2021-10-13 18:01:35 -0300

    General Distribution Status Report Included

--- ActionPlans/Start-SPORetentionChecker.ps1
+++ ActionPlans/Start-SPORetentionChecker.ps1
@@ -8,29 +8,40 @@ Description:
 <#
 
 ok 1 - Enumerate common issues this script may spot
-2 - Add output to highlight the issues and guide fixes
-3 - Integrate with main menu
-4 - Integrate with S&C connection function
-5 - Create wiki for the Action Plan
-    word document
-x - Automate adding exception for a site?
-x - Automate adding exception for a M365 group?
+   2 - Add output to highlight the issues and guide fixes
+ok 3 - Integrate with main menu
+ok 4 - Integrate with S&C connection function --> Pending function fix worked by Victor
+   5 - Code the issues checks
+   6 - Create wiki for the Action Plan
+   7 - Create the help info using the template
+   x - Automate adding exception for a site?
+   x - Automate adding exception for a M365 group?
 
 1 - Enumerate common issues this script may spot
-  ok  a. Sharepoint holds over ODB sites
-  ok  b. M365 group holds over SPO sites
-    c. Distribution issues checking status for Mode, DistributionStatus, DistributionResults
-    d. Show policies protecting a site
+    ok  a. Sharepoint holds over ODB sites
+    ok  b. M365 group holds over SPO sites
+        c. Distribution issues checking status for Mode, DistributionStatus, DistributionResults
+        d. Show policies protecting a site --> Create issue for later (not in hackathon sprint) 
 
 2 - Add output to highlight the issues and guide fixes
-    a. Add references about how to setup the exclusions
+        a. Add references about how to setup the exclusions
 
+5 - Code the issues checks
+    ok  a. Sharepoint holds over ODB sites
+    ok  b. M365 group holds over SPO sites
+        c. Distribution issues checking status for Mode, DistributionStatus, DistributionResults
+
+6 - Create wiki for the Action Plan
+    Word document + share with Victor
+    https://answers.microsoft.com/en-us/msoffice/forum/all/how-to-diagnose-invalid-public-folder-dumpster/55d5fdfc-9309-4764-9b2e-5466bedc5227
+    https://answers.microsoft.com/en-us/msoffice/forum/all/validating-distribution-group-eligibility-for/dd3a2271-cb97-4579-8935-19409dab1dc2
+    https://answers.microsoft.com/en-us/msoffice/forum/all/creating-aad-connect-rules-to-synchronize-on/41444825-f62f-4f1a-a449-152806319568
 
 Questions:
-    1 - Field order in report
-    2 - Export CSV should be optional?
-    3 - Make the report more inclusive
-    4 - PS session crashing --> Due to the module design
+    Closed   1 - Field order in report --> Victor will either create a switch or let it respect the object order
+    Closed   2 - Export CSV should be optional? --> Export when not empty
+    Closed   3 - Make the report more inclusive including signs rather than colors only --> We'll create an 'issue' to build this after the hackathon 
+    Closed   4 - PS session crashing --> Due to the module design, it won't happen in prod experience
     
 #>
 #endregion ------------------------------------------------------------
@@ -44,7 +55,7 @@ Connect-IPPSSession -UserPrincipalName roan@roanmarques.onmicrosoft.com -Prefix 
 
 #Connect-O365PS "SCC"
 #Connect-IPPSSession -Prefix cc
-Connect-IPPSSession -UserPrincipalName roan@roanmarques.onmicrosoft.com -Prefix cc
+#Connect-IPPSSession -UserPrincipalName roan@roanmarques.onmicrosoft.com -Prefix cc
 
 
 # Create the Export Folder
@@ -89,12 +100,15 @@ Foreach ($workload in $workloads){
             # Treat the policies where the scope is the whole workloads, and following check for exceptions
             If ($P.$($workload + "Location").Name -eq "All") {
                 $ReportLine = [PSCustomObject]@{
-                  PolicyName = $P.Name
-                  SiteName   = "All $workload Sites"
-                  Address    = "All $workload Sites"
-                  Workload   = "$workload"
-                  #Type       = $P.Type #currently useless
-                  Guid       = $P.Guid
+                  PolicyName          = $P.Name
+                  SiteName            = "All $workload Sites"
+                  Address             = "All $workload Sites"
+                  Workload            = $workload
+                  Type                = $P.Type
+                  Guid                = $P.Guid
+                  Mode                = $P.Mode
+                  DistributionStatus  = $P.DistributionStatus
+                  DistributionResults = $P.DistributionResults
                   }
                 $Report += $ReportLine 
 
@@ -102,14 +116,17 @@ Foreach ($workload in $workloads){
                 If ($P.$($workload + "LocationException").count -gt 0) {
                     $Locations = ($P | Select-Object -ExpandProperty $($workload + "LocationException"))
                         ForEach ($L in $Locations) {
-                            $Exception = "*Exclude* " + $L.DisplayName
+                            $Exception = "[EXCLUDE] " + $L.DisplayName
                             $ReportLine = [PSCustomObject]@{
-                                            PolicyName = $P.Name
-                                            SiteName   = $Exception
-                                            Address    = $L.Name
-                                            Workload   = "$workload"
-                                            #Type       = $P.Type #currently useless
-                                            Guid       = $P.Guid
+                                            PolicyName          = $P.Name
+                                            SiteName            = $Exception
+                                            Address             = $L.Name
+                                            Workload            = $workload
+                                            Type                = $P.Type
+                                            Guid                = $P.Guid
+                                            Mode                = $P.Mode
+                                            DistributionStatus  = $P.DistributionStatus
+                                            DistributionResults = $P.DistributionResults
                                         }
                             $Report += $ReportLine
                         }
@@ -121,12 +138,15 @@ Foreach ($workload in $workloads){
                 $Locations = ($P | Select-Object -ExpandProperty $($workload + "Location"))
                 ForEach ($L in $Locations) {
                     $ReportLine = [PSCustomObject]@{
-                                    PolicyName = $P.Name
-                                    SiteName   = $L.DisplayName
-                                    Address    = $L.Name
-                                    Workload   = "$workload"
-                                    #Type       = $P.Type #currently useless
-                                    Guid       = $P.Guid
+                                    PolicyName          = $P.Name
+                                    SiteName            = $L.DisplayName
+                                    Address             = $L.Name
+                                    Workload            = $workload
+                                    Type                = $P.Type
+                                    Guid                = $P.Guid
+                                    Mode                = $P.Mode
+                                    DistributionStatus  = $P.DistributionStatus
+                                    DistributionResults = $P.DistributionResults
                                     }
                     $Report += $ReportLine  
                 }                    
@@ -134,20 +154,24 @@ Foreach ($workload in $workloads){
     }
 }
 
+$HoldsReport = $Report | Select-Object PolicyName, SiteName, Address, Workload, Type, Guid
+$PoliciesReport = $Report | 
+                    Sort-Object Guid -Unique | 
+                        Select-Object PolicyName, Type, Guid, Mode, DistributionStatus, DistributionResults
+          
+
 # Filter SharePoint Holds - SharePoint + M365 groups
-$SPOReport = $Report | Where-Object {$_.Workload -in @("SharePoint","ModernGroup")}
+$SPOReport = New-Object -TypeName "System.Collections.ArrayList"
+$SPOReport = $HoldsReport | Where-Object {$_.Workload -in @("SharePoint","ModernGroup")} 
 $SPOReport = $SPOReport | Sort-Object -Descending "SiteName" | Sort-Object "PolicyName"
 
 # Filter OneDrive Holds - SharePoint and OneDrive except policies expliciting only SharePoint Sites
 $ODBReport = New-Object -TypeName "System.Collections.ArrayList"
-$ODBReport = $Report | Where-Object {$_.Workload -eq "SharePoint" -and $_.Address -notmatch "sharepoint.com"}
-$ODBReport += $Report | Where-Object {$_.Workload -eq "SharePoint" -and $_.Address -match "-my.sharepoint.com"}
-$ODBReport += $Report | Where-Object {$_.Workload -eq "OneDrive"}
+$ODBReport = $HoldsReport | Where-Object {$_.Workload -eq "SharePoint" -and $_.Address -notmatch "sharepoint.com"}
+$ODBReport += $HoldsReport | Where-Object {$_.Workload -eq "SharePoint" -and $_.Address -match "-my.sharepoint.com"}
+$ODBReport += $HoldsReport | Where-Object {$_.Workload -eq "OneDrive"}
 $ODBReport = $ODBReport | Sort-Object -Descending "SiteName" | Sort-Object "PolicyName"
 
-
-# If specified, exports the report for an CSV file  
-$Report | Export-Csv -NoTypeInformation $ExportPath\SPOTenantHoldsReport.csv -Encoding UTF8
 
 #region Prepare HTML Report
 $TheObjectToConvertToHTML = New-Object -TypeName "System.Collections.ArrayList"
@@ -156,8 +180,16 @@ $TheObjectToConvertToHTML = New-Object -TypeName "System.Collections.ArrayList"
 
 # Adds general guidance about the report
 [string]$SectionTitle = "Report Guidance"
+#TODO: Break in multiple lines
 [string]$Description = "A Site/OneDrive explicitly included is protected. `nA Site/OneDrive included by an 'All' workload policy is protected if not explicitly excluded by the same policy. `nInclusion policies precede exclusion policies."
 [PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -DataType "String" -EffectiveDataString $Description
+$null = $TheObjectToConvertToHTML.Add($SectionHtml)
+
+# Adds the session for distribution status
+#TODO: check if there is content
+[string]$SectionTitle = "Policies Distribution Status"
+[string]$Description = "Check the status for each policy distribution"
+[PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $PoliciesReport -TableType Table
 $null = $TheObjectToConvertToHTML.Add($SectionHtml)
 
 # Adds the session for policies affecting SharePoint
@@ -170,6 +202,7 @@ $null = $TheObjectToConvertToHTML.Add($SectionHtml)
 $null = $TheObjectToConvertToHTML.Add($SectionHtml)
 
 # Adds the session for policies affecting OneDrive
+#TODO: check if there is content
 [string]$SectionTitle = "OneDrive Holds"
 [string]$Description = "These are the holds which may be preventing OneDrive files and sites to be deleted."
 [PSCustomObject]$SectionHtml = Prepare-ObjectForHTMLReport -SectionTitle $SectionTitle -SectionTitleColor "Black" -Description $Description -DataType "CustomObject" -EffectiveDataArrayList $ODBReport -TableType Table
