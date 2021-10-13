591af9744f5909e7ee3ff8ee96d1aa663c984dcd
hazemembaby <71065889+hazemembaby@users.noreply.github.com>
2021-10-13 11:45:01 +0200

    modify the RBAC function

--- ActionPlans/Start-RbacTools.ps1
+++ ActionPlans/Start-RbacTools.ps1
@@ -6,8 +6,8 @@ Function Get-SpecificRoleMembers {
         $roles = (Get-ManagementRole | Select-Object name | Sort-Object name  |Out-GridView -PassThru -Title "List users that have the role you selected:").Name
     }
     $GetEffectiveUsers = Get-ManagementRoleAssignment -GetEffectiveUsers | Where-Object {(($_.Enabled -eq $True) -and ($roles -match $_.Role))} |`
-        Select-Object Role, RoleAssigneeName, RoleAssigneeType, RoleAssignmentDelegationtype, @{ Name = 'Alias(User)'; Expression = {$_.User}}, CustomRecipientWriteScope, CustomConfigWriteScope, RecipientWriteScope, ConfigWriteScope, Identity 
-    return $GetEffectiveUsers
+        Select-Object  Role, RoleAssigneeName, RoleAssigneeType, RoleAssignmentDelegationtype, @{ Name = 'Alias(User)'; Expression = {$_.User}}, CustomRecipientWriteScope, CustomConfigWriteScope, RecipientWriteScope, ConfigWriteScope, Identity
+        return $GetEffectiveUsers
 }
 
 Function Get-AllUsersWithAllRoles {
